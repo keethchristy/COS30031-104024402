f1a46193510ef4cb360128177b66863e1082ed50
Keeth <104024402@student.swin.edu.au>
2024-07-31 13:48:50 +1000

    huh
    test

---
+++ 02c - Lab - C++ For Programmers/Lab2-Part1/SimpleProgram.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	chickennuggies(1, 2);
+}
+
+void chickennuggies(int chicken, int nuggie) {
+	cout << "I have" << chicken << "chickens and" << nuggie << "nuggets";
+	cout << "Hello World";
+}
