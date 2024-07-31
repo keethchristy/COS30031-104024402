72e59968bd7e23b40f85abd5a4ea389e6d0228a0
Keeth <104024402@student.swin.edu.au>
2024-07-31 14:04:49 +1000

    parts of q1
    1A, 1B, 1D

--- 02c - Lab - C++ For Programmers/Lab2-Part1/SimpleProgram.cpp
+++ 02c - Lab - C++ For Programmers/Lab2-Part1/SimpleProgram.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	chickennuggies(1, 2);
+
+void chickennuggies(int chicken, int nuggies) {
+	cout << "I have " << chicken << " chicken and " << nuggies << " nuggies";
 }
 
-void chickennuggies(int chicken, int nuggie) {
-	cout << "I have" << chicken << "chickens and" << nuggie << "nuggets";
-	cout << "Hello World";
+int thisfunctionaltersstuff(int alterme) {
+	return alterme + 5;
 }
+
+void loopfor20() {
+	for (int i = 0; i <= 20; i++) {
+		if (i % 2 == 1) {
+			cout << i << "  ";
+		}
+	}
+}
+
+
+int main() {
+	//1A
+	chickennuggies(1, 2);
+	//1B
+	cout << "\n" << thisfunctionaltersstuff(5);
+	//
+	// 1C: Pointers - DO LATER, CANT BE ASSED
+	//
+	cout << "\n";
+	//1D
+	loopfor20();
+	//1E
+
+}
+
+
